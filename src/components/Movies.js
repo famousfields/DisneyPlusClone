72d79c80227e28be13b9476e47a7b71b46ea2e7c
famousfields <willfields92@gmail.com>
2023-06-02 19:57:26 -0400

    completed home screen

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -8,7 +8,30 @@ function Movies() {
             Reccomended for You
         </h4>
         <Content>
-
+            <Wrap>
+                <img src = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
         </Content>
     </Container>
   )
@@ -20,5 +43,30 @@ const Container = styled.div`
     
 `
 const Content = styled.div`
-    
+    display: grid;
+    grid-gap: 25px;
+    grid-template-columns: repeat(4,minmax(0,1fr));
+`
+
+const Wrap = styled.div`
+    cursor: pointer;
+    border-radius: 10px;
+    overflow: hidden;
+    border: 3px solid rgba(249,249,249,.1);
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, 
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition: all 250ms cubic-bezier(0.25, 0.46,.45,.94) 0s;
+     
+    img{
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+    }
+    &:hover
+    {
+        transform: scale(1.05);
+        border-color: rgba(249,249,249,.8);
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, 
+        rgb(0 0 0 / 73%) 0px 30px 22px -10px;
+    }
 `
