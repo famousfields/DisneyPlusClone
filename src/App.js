6a2a64dedca24e470e5169eb734b37da71302518
famousfields <willfields92@gmail.com>
2023-06-02 20:43:47 -0400

    add Details page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+
+import { BrowserRouter as Router,Routes, Route,Link  } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+          <Header />
+          <Routes>
+            <Route path = "/detail" element ={<Detail/>} /> 
+            <Route exact path = "/" element = {<Home/>}/>
+          </Routes>
+      </div>
+    </Router>
+
   );
 }
 
